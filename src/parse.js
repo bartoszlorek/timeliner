d6cd46acd9c93b07905fc04434893f2d6a1aee72
Bartosz Lorek <bery.lorek@gmail.com>
2017-05-02 21:42:15 +0200

    parse dynamic groups instead of simple cards

--- src/parse.js
+++ src/parse.js
@@ -1,77 +1,65 @@
-import { forEach } from './utils.js';
+import { forEach, indexOf } from './utils/utils.js';
+import { query } from './utils/dom.js';
 
-const CARD_SELECTOR = '.todo-taskboard-card:not(.todo-taskboard-card-completed)';
+const GROUP_SELECTOR = '.column';
+const TITLE_SELECTOR = '.title';
 const TODO_SELECTOR = '.todo-checklist-item:not(.todo-hidden)';
-const BUCKET_SELECTOR = '.todo-taskboard-card-bucketName';
-const TOPBAR_SELECTOR = '.todo-taskboard-card-topBar';
-
-// dd.mm
-// dd.mm.yyyy
-// dd.mm-dd.mm
-// dd.mm-dd.mm.yyyy
-// dd.mm.yyyy-dd.mm.yyyy
 const DATE_REGEX = /^(\d{1,2})\.?(\d{2})?\.?(\d{4})?\s*\-?\s*(\d{1,2})?\.?(\d{2})?\.?(\d{4})?/;
 const YEAR = new Date().getFullYear();
 
 export default function(content) {
-    let cards = content.querySelectorAll(CARD_SELECTOR),
+    let groups = query(GROUP_SELECTOR, content),
         result = [],
         data;
-    forEach(cards, (i, card) => {
-        data = cardData(card);
-        if (data !== false) {
+    forEach(groups, (i, group) => {
+        if (data = groupData(group)) {
             result.push(data);
         }
     });
     return result;
 }
 
-function cardData(card) {
-    let topBar = card.querySelector(TOPBAR_SELECTOR);
-    if (topBar !== null) {
-        let todos = todosTerms(cardTodos(topBar));
-        if (todos.length) {
-            return {
-                bucket: topBar
-                    .querySelector(BUCKET_SELECTOR)
-                    .textContent,
-                terms: todos
-            }
-        } 
+function groupData(column) {
+    let terms = validTerms(query(TODO_SELECTOR, column));
+    if (terms.length) {
+        return {
+            title: query(TITLE_SELECTOR, column)[0]
+                .textContent,
+            terms
+        }
     }
     return false;
 }
 
-function cardTodos(card) {
-    let items = card.querySelectorAll(TODO_SELECTOR),
-        result = [];
-    forEach(items, (i, item) => {
-        result.push({
-            text: item
-                .children[1]
-                .textContent,
-            item
-        });
-    });
-    return result;
-}
-
-function todosTerms(todos) {
-    let result = [],    
+function validTerms(todos) {
+    let result = [],  
         date;
     forEach(todos, (i, todo) => {
-        if (date = parseDate(todo.text)) {
+        date = parseDate(todo
+            .children[1]
+            .textContent);
+
+        if (date) {
             result.push({
-                item: todo.item,
-                date 
+                todo,
+                date
             });
         }
     });
     return result;
 }
 
+// possible date format:
+// dd.mm.yyyy-dd.mm.yyyy
+// dd.mm-dd.mm.yyyy
+// dd.mm-dd.mm
+// dd.mm.yyyy
+// dd.mm
+
+// regex returns:
+// [full, dd, mm, yyyy, ddd, mm, yyyy]
+
 function parseDate(string) {
-    // [full, dd, mm, yyyy, ddd, mm, yyyy]
     let match = DATE_REGEX.exec(string);
     if (match === null) {
         return false;
