93ab74abc9cba9e3232085cce954fd3173cccae4
Bartosz Lorek <bery.lorek@gmail.com>
2017-05-03 22:59:54 +0200

    parse card's todos or simple due date

--- src/parse.js
+++ src/parse.js
@@ -3,7 +3,11 @@ import { query } from './utils/dom.js';
 
 const GROUP_SELECTOR = '.column';
 const TITLE_SELECTOR = '.title';
+const CARD_SELECTOR = '.todo-taskboard-card:not(.todo-taskboard-card-completed)';
+const CARD_TITLE_SELECTOR = '.todo-taskboard-card-title';
 const TODO_SELECTOR = '.todo-checklist-item:not(.todo-hidden)';
+const DATE_SELECTOR = '.todo-taskboard-card-dueDate';
+
 const DATE_REGEX = /^(\d{1,2})\.?(\d{2})?\.?(\d{4})?\s*\-?\s*(\d{1,2})?\.?(\d{2})?\.?(\d{4})?/;
 const YEAR = new Date().getFullYear();
 
@@ -20,15 +24,30 @@ export default function(content) {
 }
 
 function groupData(column) {
-    let terms = validTerms(query(TODO_SELECTOR, column));
-    if (terms.length) {
-        return {
-            title: query(TITLE_SELECTOR, column)[0]
-                .textContent,
-            terms
+    let cards = query(CARD_SELECTOR, column),
+        title = query(TITLE_SELECTOR, column)[0].textContent,
+        result = [],
+        terms,
+        date;
+    forEach(cards, (i, card) => {
+        terms = validTerms(query(TODO_SELECTOR, card));
+        if (terms.length) {
+            result = result.concat(terms);
         }
+        else if (date = cardDueDate(card)) {
+            result.push({
+                entry: card,
+                date
+            });
+        }
+    });
+    if (!result.length) {
+        return false;
     }
-    return false;
+    return {
+        terms: result,
+        title
+    }
 }
 
 function validTerms(todos) {
@@ -41,7 +60,7 @@ function validTerms(todos) {
 
         if (date) {
             result.push({
-                todo,
+                entry: todo,
                 date
             });
         }
@@ -67,8 +86,8 @@ function parseDate(string) {
     if (!match[4]) {
         match[3] = match[3] || YEAR;
         return newDate([match[1], match[2], match[3]]);
-
-    } else {
+    }
+    else {
         match[2] = match[2] || match[5];
         match[3] = match[3] || match[6] || YEAR;
         match[6] = match[6] || YEAR;
@@ -96,6 +115,20 @@ function newDate(array) {
         parseInt(array[2], 10),
         month - 1,
         day,
+        0, 0, 0, 0
+    );
+}
+
+function cardDueDate(card) {
+    let date = query(DATE_SELECTOR, card)[0].title;
+    if (!date) {
+        return false;
+    }
+    date = date.split('-');
+    return new Date(
+        date[0],
+        parseInt(date[1], 10) - 1,
+        date[2],
         0, 0, 0, 0
     );
 }
